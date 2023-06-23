3508d9c6bb38931f5f6986a5021827810fceedc4
Danilo Teixeira <104468545+danilot1@users.noreply.github.com>
2023-06-23 12:58:59 -0300

    Add files via upload

---
+++ operadores_aritmeticos_2_ aula3.2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+	
+	int dado = 10;
+	printf("Dado antes do incremento: %i \n",dado);
+	
+	dado ++;
+	printf("Dado depois do incremento: %i \n",dado);
+	
+	dado --;
+	printf("Dado depois do decremento: %i \n",dado);
+	
+	dado += 3;
+	printf("Dado depois do incremento em 3 unidades: %i \n",dado);
+	
+	dado -= 3;
+	printf("Dado depois do decremento em 3 unidades: %i \n",dado);
+	
+	dado *=10;
+	printf("Dado depois de multiplicar por 10: %i \n",dado);
+	
+	
+}
---
+++ operadores_aritmeticos_aula3.2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+ int main(){
+	
+	int A, B, soma, subtr, multipl, divis;
+	
+	printf("Digite o primeiro valor:\n");
+	scanf("%d",&A);
+	printf("Digite o segundo valor: \n");
+	scanf("%d",&B);
+	
+	soma = A + B;
+	subtr = A - B;
+	multipl = A * B;
+	divis = A / B;
+	
+	printf("Resultados: \n");
+	printf("Soma: %d \n",soma);
+	printf("Subtracao: %d \n",subtr);
+	printf("Multiplicacao: %d \n",multipl);
+	printf("Divisao: %d \n", divis);
+	
+	
+	
+	
+ }
