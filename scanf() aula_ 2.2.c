11f19244346fa0d6a89f583001983a08fb637100
Danilo Teixeira <104468545+danilot1@users.noreply.github.com>
2023-06-23 11:44:39 -0300

    Add files via upload

---
+++ scanf() aula_ 2.2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+		
+		int idade = 0;
+		int ano = 5;
+		
+		printf("Valor inicial da idade  %d.\n",idade);
+		
+		printf("Digite uma idade e o ano :\n");
+		scanf("%d %d", &idade, &ano);
+		
+		printf("Idade informada: %d.\n", idade);
+		printf("Ano informado: %d.\n", ano);
+		
+}
