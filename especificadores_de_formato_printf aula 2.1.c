6fb8755be54936b6bc46c717134051e551e96c9d
Danilo Teixeira <104468545+danilot1@users.noreply.github.com>
2024-02-19 16:36:29 -0300

    Update especificadores_de_formato_printf aula 2.1.c

--- especificadores_de_formato_printf aula 2.1.c
+++ especificadores_de_formato_printf aula 2.1.c
@@ -2,7 +2,7 @@
 
 int main(){
 	
-	printf("Oi, tudo bem? tenho 6 anos e programo.\n");
+	printf("Oi, tudo bem?.\n");
 	printf("Valor inteiro: %d \n", 10);
 	printf("Valor real: %f\n", 3.14159265);
 	printf("Valor real com apenas duas casas: %.2f\n", 3.14159265);	
